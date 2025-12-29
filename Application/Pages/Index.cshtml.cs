2ab23eda5462d6aa69c713006f99371bb28e9045
teriket <tanner.h.hunt@gmail.com>
2025-12-29 16:14:19 -0700

    imported C#/Razor application template

--- Application/Pages/Index.cshtml.cs
+++ Application/Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Application.Pages;
 
@@ -14,6 +13,7 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-
+        string dateTime = DateTime.Now.ToString();
+        ViewData["TimeStamp"] = dateTime;
     }
 }
